110f03a7ab5870e18d9bbf3ad6c7c49f599b4546
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 11:47:17 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outer.Inner myInner = new Outer.Inner();
+
+        System.out.println(myInner.y);
+
+
+    }
+
+}
